96707685b30e058c009d858316eed47a18713d8c
O'Brien <abucheriobrien@gmail.com>
2023-11-13 00:30:17 +0000

    test_console update

--- tests/test_console.py
+++ tests/test_console.py
@@ -47,6 +47,42 @@ class TestConsole(unittest.TestCase):
             output = f.getvalue()
             self.assertIn(obj_id, output)
 
+    def test_destroy_command(self):
+        """Test the destroy command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+            HBNBCommand().onecmd(f"destroy BaseModel {obj_id}")
+            self.assertNotIn(obj_id, storage.all())
+
+    def test_all_command(self):
+        """Test the all command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            HBNBCommand().onecmd("create User")
+            HBNBCommand().onecmd("all")
+            output = f.getvalue()
+            self.assertIn("BaseModel", output)
+            self.assertIn("User", output)
+
+    def test_update_command(self):
+        """Test the update command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+            HBNBCommand().onecmd(f"update BaseModel {obj_id} name 'new_name'")
+            updated_obj = storage.all()[f"BaseModel.{obj_id}"]
+            self.assertEqual(updated_obj.name, "new_name")
+
+    def test_count_command(self):
+        """Test the count command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            HBNBCommand().onecmd("create BaseModel")
+            HBNBCommand().onecmd("count BaseModel")
+            output = f.getvalue()
+            self.assertEqual(output.strip(), "2")
+
     def test_non_interactive_help_command(self):
         """Test non-interactive mode with help command"""
         command = "echo 'help' | ./console.py"
@@ -54,6 +90,55 @@ class TestConsole(unittest.TestCase):
             output = p.read()
             self.assertIn("Documented commands", output)
             self.assertIn("quit", output)
+
+    def test_non_interactive_create_command(self):
+        """Test non-interactive mode with create command"""
+        command = "echo 'create BaseModel' | ./console.py"
+        with os.popen(command) as p:
+            output = p.read().strip()
+            self.assertTrue(output.isdigit())
+
+    def test_non_interactive_destroy_command(self):
+        """Test non-interactive mode with destroy command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+            command = f"echo 'destroy BaseModel {obj_id}' | ./console.py"
+            with os.popen(command) as p:
+                output = p.read()
+                self.assertNotIn(obj_id, storage.all())
+
+    def test_non_interactive_all_command(self):
+        """Test non-interactive mode with all command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            HBNBCommand().onecmd("create User")
+            command = "echo 'all' | ./console.py"
+            with os.popen(command) as p:
+                output = p.read()
+                self.assertIn("BaseModel", output)
+                self.assertIn("User", output)
+
+    def test_non_interactive_update_command(self):
+        """Test non-interactive mode with update command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            obj_id = f.getvalue().strip()
+            command = f"echo 'update BaseModel {obj_id} name new_name' \
+                    | ./console.py"
+            with os.popen(command) as p:
+                updated_obj = storage.all()[f"BaseModel.{obj_id}"]
+                self.assertEqual(updated_obj.name, "new_name")
+
+    def test_non_interactive_count_command(self):
+        """Test non-interactive mode with count command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            HBNBCommand().onecmd("create BaseModel")
+            command = "echo 'count BaseModel' | ./console.py"
+            with os.popen(command) as p:
+                output = p.read().strip()
+                self.assertEqual(output, "2")
 
 
 if __name__ == '__main__':
